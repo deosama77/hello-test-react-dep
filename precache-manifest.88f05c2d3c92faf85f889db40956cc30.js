7aff974c4402986d6c979097c68f00210296bce8
deosama77 <deosama77@gmail.com>
2020-07-28 11:18:43 +0300

    Updates

---
+++ precache-manifest.88f05c2d3c92faf85f889db40956cc30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5af876fd77994866afd27a56f345ff",
+    "url": "/deosama77/hello-test-react-dep/index.html"
+  },
+  {
+    "revision": "86fdcfe8db96e4de9b8a",
+    "url": "/deosama77/hello-test-react-dep/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "efc498ad919cf964afcb",
+    "url": "/deosama77/hello-test-react-dep/static/js/2.36ab9417.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deosama77/hello-test-react-dep/static/js/2.36ab9417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86fdcfe8db96e4de9b8a",
+    "url": "/deosama77/hello-test-react-dep/static/js/main.df9f2004.chunk.js"
+  },
+  {
+    "revision": "55139f5cf69e488bd87d",
+    "url": "/deosama77/hello-test-react-dep/static/js/runtime-main.96ef10ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deosama77/hello-test-react-dep/static/media/logo.5d5d9eef.svg"
+  }
+]);
